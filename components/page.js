36ba7f238993cfdf18e59c330d70b5ad2399ab67
丛林意志 <2523090520@qq.com>
2021-03-17 19:31:26 +0800

    增加注释

--- components/page.js
+++ components/page.js
@@ -6,15 +6,19 @@ let debug = false;
 module.exports = {
     type:'list',
     async fetch({ args }) {
+        // 隐藏启动页
         $prefs.set('showWelcome',false)
         if(debug) console.log('Loading.....')
         let form = args.form.split(";");
         let View = []
         console.log(args.header)
+        // 通过 axios 访问网络并且获取数据
+        // 因为在无网络的情况下基本无法使用整个插件，所以考虑不做异常捕捉
         httpReturn = await axios[form[0]||'get'](form[2],{
             headers:args.headers||{}
         });
         let data = httpReturn.data;
+        // HTML 解析部分，没有完工
         if(form[1]=='html'){
             $ = require('cheerio').load(data);
             return $(args.itemSelect).map((index,item)=>{
@@ -48,6 +52,7 @@ module.exports = {
                 return card;
             })
         }else if(form[1]=='json'){
+            // JSON 解析模式，大多数情况下他们可以按照预期工作
             args.itemRoot.split('.').forEach(key=>{
                 data = data[key]
             })
@@ -55,6 +60,7 @@ module.exports = {
                 console.log(`Debug:输出数组成员 > ${data.length}`)
                 console.log(data);
             }
+            // Dora.js 标题设置为空，这样可以让顶栏好看一点，然而实际 1.9.0 并没有实际作用……
             this.title=''
             View = [].concat(data.map((cofing,index)=>{
                 function _template(mod){
@@ -76,7 +82,7 @@ module.exports = {
                 })
                 card.author.avatar = (args.author_avatar)?_template(args.author_avatar):''
                 card.author.name = (args.author_name)?_template(args.author_name):''
-
+                // 转换点击执行或者长按执行
                 let fun = $prefs.get('clickToGo')?'onClick':'onLongClick'
                 if(args.schame) card[fun]=()=>{
                     $router.to($route(_template(args.schame)))
