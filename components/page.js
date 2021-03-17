f3021e869a089cb86f9f8abd697afaf401213a64
丛林意志 <2523090520@qq.com>
2021-03-17 20:17:40 +0800

    压缩代码和优化逻辑

--- components/page.js
+++ components/page.js
@@ -1,7 +1,7 @@
 let axios = $http
 let template = require('art-template');
 let httpReturn
-let debug = false;
+let debug = true;
 
 module.exports = {
     type:'list',
@@ -10,7 +10,6 @@ module.exports = {
         $prefs.set('showWelcome',false)
         if(debug) console.log('Loading.....')
         let form = args.form.split(";");
-        let View = []
         console.log(args.header)
         // 通过 axios 访问网络并且获取数据
         // 因为在无网络的情况下基本无法使用整个插件，所以考虑不做异常捕捉
@@ -22,7 +21,7 @@ module.exports = {
         if(form[1]=='html'){
             $ = require('cheerio').load(data);
             return $(args.itemSelect).map((index,item)=>{
-                let itemData = {
+                let cofing = {
                     __index:index
                 };
                 for (const key in args.dataSelect) {
@@ -31,11 +30,12 @@ module.exports = {
                     select.forEach(_select=>{
                         DOM = DOM.children(_select)
                     })
-                    itemData[key] = DOM.text()
+                    cofing[key] = DOM.text()
                 }
-                if(debug)console.log(itemData);
+                if(debug)console.log(cofing);
+                // 从下面开始的东西应该调用 makeCard() 去执行……
                 function _template(t){
-                    return template.render(t,itemData)
+                    return template.render(t,cofing)
                 }
                 let card = {};
                 card.style = args.style||'simple';
@@ -49,53 +49,65 @@ module.exports = {
                     let _key_1 = args[_key[1]||_key[0]]
                     card[_key[0]] = (_key_1)?_template(_key_1):_key[2]
                 });
-                return card;
+                // return makeCard({args,cofing,index});
+                return card
             })
         }else if(form[1]=='json'){
             // JSON 解析模式，大多数情况下他们可以按照预期工作
             args.itemRoot.split('.').forEach(key=>{
                 data = data[key]
             })
+
             if(debug){
                 console.log(`Debug:输出数组成员 > ${data.length}`)
                 console.log(data);
             }
             // Dora.js 标题设置为空，这样可以让顶栏好看一点，然而实际 1.9.0 并没有实际作用……
-            this.title=''
-            View = [].concat(data.map((cofing,index)=>{
-                function _template(mod){
-                    return template.render(mod,cofing)
-                }
-                cofing.__index = index
-                let card = {};
-                card.style = args.style||'list';
-                card.author = {};
-                [
-                    ['image','',args.icon],
-                    ['viewerCount','',''],
-                    ['label','',''],
-                    ['summary','',''],
-                    ['title','','']
-                ].forEach(key=>{
-                    let key_1 = args[key[1]||key[0]]
-                    card[key[0]] = (key_1)?_template(key_1):key[2]
-                })
-                card.author.avatar = (args.author_avatar)?_template(args.author_avatar):''
-                card.author.name = (args.author_name)?_template(args.author_name):''
-                // 转换点击执行或者长按执行
-                let fun = $prefs.get('clickToGo')?'onClick':'onLongClick'
-                if(args.schame) card[fun]=()=>{
-                    $router.to($route(_template(args.schame)))
-                }
-                else if(args.video) card[fun]=()=>{
-                    $router.to($route('video',{url:_template(args.video)}))
-                }
-                else if(args.url) card[fun]=()=>{
-                    $router.to($route('webview',{url:_template(args.url)}))
-                }
-                return card
-            }))
-            return View;
+            // this.title=''
+
+            return data.map((cofing,index)=>{
+                return makeCard({args,cofing,index})
+            });
         }
     }
+}
+
+
+function makeCard({args,cofing,index}){
+    function _template(mod){
+        return mod?template.render(mod,cofing):''
+    }
+    let card = {
+        style:args.style||'list',
+        author:{
+            avatar:_template(args.author_avatar),
+            name:_template(args.author_name)
+        }
+    };
+
+    cofing.__index = index;
+
+    [
+        ['image',args.icon],
+        ['viewerCount',''],
+        ['label',''],
+        ['summary',''],
+        ['title','']
+    ].forEach(key=>{
+        let i = key[0]
+        card[i] = _template(args[i])
+    })
+
+    // 转换点击执行或者长按执行
+    let fun = $prefs.get('clickToGo')?'onClick':'onLongClick'
+    if(args.schame) card[fun]=()=>{
+        $router.to($route(_template(args.schame)))
+    }
+    else if(args.video) card[fun]=()=>{
+        $router.to($route('video',{url:_template(args.video)}))
+    }
+    else if(args.url) card[fun]=()=>{
+        $router.to($route('webview',{url:_template(args.url)}))
+    }
+    return card
 }
