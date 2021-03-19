2ef293ef055592bf84872b01a39d85d7203e28c4
丛林意志 <2523090520@qq.com>
2021-03-19 18:04:23 +0800

    被欺负的没辙写的打包工具

---
+++ bulid.bat
@@ -0,0 +1,9 @@
+7z a package.tar "package/assets/*" 
+7z a package.tar "package/components/*" 
+7z a package.tar "package/main.js" 
+7z a package.tar "package/package-lock.json" 
+7z a package.tar "package/package.json" 
+7z a package.tar "package/README.md" 
+7z a package.tar "package/yarn.lock" 
+7z a package.tar.gz "package.tar"
+rename package.tar.gz package.dora
