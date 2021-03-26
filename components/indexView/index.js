b9bff26bc7fe6043127e942cf7019b93abd1ec4b
丛林意志 <2523090520@qq.com>
2021-03-26 23:22:45 +0800

    增加一个文件

--- components/indexView/index.js
+++ components/indexView/index.js
@@ -3,7 +3,8 @@ module.exports = {
     fetch() {
         return [
             {title:'[用户]快速使用文档',route:$route('indexView/show',{mod:'user'})},
-            {title:'[开发]源编写文档',route:$route('indexView/show',{mod:'dev'})}
+            {title:'[开发]源编写文档',route:$route('indexView/show',{mod:'dev'})},
+            {title:'[特例]不同框架的特性',route:$route('indexView/show',{mod:'framework'})}
         ]
     }
 }
