68587e7b91ce6ef37ab4b694e3a4c8d365f24c52
丛林意志 <42381347+conglinyizhi@users.noreply.github.com>
2021-03-27 11:29:22 +0800

    压缩代码

--- components/indexView/index.js
+++ components/indexView/index.js
@@ -2,9 +2,15 @@ module.exports = {
     type: 'topTab',
     fetch() {
         return [
-            {title:'[用户]快速使用文档',route:$route('indexView/show',{mod:'user'})},
-            {title:'[开发]源编写文档',route:$route('indexView/show',{mod:'dev'})},
-            {title:'[特例]不同框架的特性',route:$route('indexView/show',{mod:'framework'})}
+            drawRoute("[用户]快速使用文档",'user'),
+            drawRoute("[开发]源编写文档",'dev'),
+            drawRoute("[特例]不同框架的特性",'framework'),
+            {title:'[配置]设置页面？',route:$route('indexView/config')}
+
         ]
     }
+}
+
+function drawRoute(title,mod){
+    return {title,route:$route('indexView/show',{mod})}
 }
